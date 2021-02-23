5abdcf37a7a0c0f1249264264fc5606985a94ebd
Joerg Faschingbauer <jf@faschingbauer.co.at>
2021-02-23 11:59:22 +0100

    digit.py

--- digit.py
+++ digit.py
@@ -1,29 +1,32 @@
 import sys
 
-print('argv:', sys.argv)
+if len(sys.argv) != 2:
+    print('Du Depp: Programm will genau einen Parameter!')
+    sys.exit()
 
 digit = sys.argv[1]
-print('digit:', digit, 'type:', type(digit))
 
-if digit == '0':
-    print('zero')
-elif digit == '1':
-    print('one')
-elif digit == '2':
-    print('two')
-elif digit == '3':
-    print('three')
-elif digit == '4':
-    print('four')
-elif digit == '5':
-    print('five')
-elif digit == '6':
-    print('six')
-elif digit == '7':
-    print('seven')
-elif digit == '8':
-    print('eight')
-elif digit == '9':
-    print('nine')
-else:
-    print('nix gueltiges!!!!')
+if len(digit) != 1:
+    print('Du Depp: Ziffern bestehen aus einem Character, hier haben wir', len(digit), 'Characters')
+    sys.exit()
+
+try:
+    digit = int(digit)
+except ValueError:
+    print('Du Depp:', digit, 'ist keine Ziffer')
+    sys.exit()
+
+translation = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+}
+
+print(translation[digit])
